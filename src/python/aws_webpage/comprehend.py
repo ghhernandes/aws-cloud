c1df3229c47d49913159a9b61320ec7e7f13f913
Gabriel Hernandes <ghh.hernandes@gmail.com>
2020-09-10 00:05:41 -0300

    run_debug and comprehend updated.

--- src/python/aws_webpage/comprehend.py
+++ src/python/aws_webpage/comprehend.py
@@ -1,6 +1,17 @@
 from aws_webpage import app 
-from flask import render_template
+from flask import render_template, request
 
-@app.route('/comprehend')
+import requests as r
+
+API_URL = "yep your api url" 
+
+@app.route('/comprehend', methods=['GET', 'POST'])
 def comprehend():
-    return render_template('main.html') 
+    if request.method == 'GET':
+        return render_template('comprehend.html') 
+    else:
+        text = str(request.form['text'])
+
+        api_request = r.post(API_URL, data={"text": [text]})
+        return render_template('comprehend.html', text=api_request.content.decode('utf8'))
+
--- src/python/run_python_debug.sh
+++ src/python/run_python_debug.sh
@@ -1,3 +1,3 @@
-
+export FLASK_APP=aws_webpage
 export FLASK_ENV=development
 flask run
